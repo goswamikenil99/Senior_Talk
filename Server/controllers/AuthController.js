d777da3af483e09b5b56083efb5430827159e5d9
goswamikenil99 <kenilgirigoswami21@gnu.ac.in>
2024-11-11 22:48:37 +0530

    commit-1

--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../model/UserModel.js";
-import { compare } from "bcrypt";
+import { compare } from "bcryptjs";
 import { renameSync, unlinkSync } from "fs";
 import nodemailer from "nodemailer";
 
